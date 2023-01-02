0c61ba59c9b07e39fafa87a577c7704d5900a183
LaVideos <lavideos1313@gmail.com>
2023-01-02 02:54:03 +0200

    navigate

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,18 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
 import {MainLayout} from "../layout";
 import {MainPage, ResumePage} from "../pages";
+import {useEffect} from "react";
 
 export const AppRouter = () => {
+    const {pathname} = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(pathname==='/cv'){
+            navigate('/cv/profile/about')
+        }
+    },[pathname])
+
     return (
         <Routes>
             <Route path={'/cv/profile'} element={<MainLayout/>}>
