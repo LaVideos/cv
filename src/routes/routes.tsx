a69478b92a57496232fd3db6dea625d6a72eea3e
LaVideos <lavideos1313@gmail.com>
2023-01-02 02:24:17 +0200

    add

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import {MainLayout} from "../layout";
 import {MainPage, ResumePage} from "../pages";
-import {AboutMeComponent, ChronoAboutComponent, Education, ExperienceComponent, HobbyComponent} from "../components";
 
 export const AppRouter = () => {
     return (
@@ -9,10 +8,6 @@ export const AppRouter = () => {
             <Route path={'/profile'} element={<MainLayout/>}>
     <Route path={'about'} index element={<MainPage/>}></Route>
     <Route path={'resume'} element={<ResumePage/>}>
-        {/*<Route path={'aboutUs'} element={<ChronoAboutComponent/>}></Route>*/}
-        {/*<Route path={'experience'} element={<ExperienceComponent/>}></Route>*/}
-        {/*<Route path={'education'} element={<Education/>}></Route>*/}
-        {/*<Route path={'hobby'} element={<HobbyComponent/>}></Route>*/}
     </Route>
     </Route>
     </Routes>
